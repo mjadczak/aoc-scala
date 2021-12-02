b343c0a8127c9d6231104aab43a46f2522555afc
Matt Jadczak <matt@mjdk.co.uk>
2021-12-02 22:20:20 +0000

    Day 1, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day1/Part2.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day1/Part2.scala
@@ -3,7 +3,7 @@ package uk.co.mjdk.aoc21.day1
 import scala.io.Source
 import scala.util.Using
 
-object Part1 {
+object Part2 {
   def main(args: Array[String]): Unit = {
     val sum = Using.resource(
       Source.fromInputStream(getClass.getResourceAsStream("input.txt"))
@@ -11,6 +11,8 @@ object Part1 {
       src
         .getLines()
         .map(_.toInt)
+        .sliding(3)
+        .map(_.sum)
         .sliding(2)
         .map {
           case Seq(a, b) if b > a => 1

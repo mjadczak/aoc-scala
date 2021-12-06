116fc060435de2a172657e3f4358d0ae3b2d3c33
Matt Jadczak <matt@mjdk.co.uk>
2021-12-06 22:49:04 +0000

    Day 5, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day05/Day05.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day05/Day05.scala
@@ -2,49 +2,53 @@ package uk.co.mjdk.aoc21.day05
 
 import uk.co.mjdk.aoc21.inputLines
 
+import math.Ordered.orderingToOrdered
+
 case class Pos(x: Int, y: Int)
+
+given Ordering[Pos] = Ordering.by((p: Pos) => p.x).orElseBy(_.y)
+
 case class Line(start: Pos, end: Pos) {
 
   val isAxisAligned: Boolean = start.x == end.x || start.y == end.y
 
   def coveredPoints: Iterator[Pos] = {
-    // Crying out for abstraction. Lenses? General case?
-    if (start.x == end.x) {
-      val (startPt, endPt) =
-        (Seq(start, end).minBy(_.y), Seq(start, end).maxBy(_.y))
-      Iterator
-        .iterate(startPt)(p => p.copy(y = p.y + 1))
-        .takeWhile(_.y <= endPt.y)
-    } else if (start.y == end.y) {
-      val (startPt, endPt) =
-        (Seq(start, end).minBy(_.x), Seq(start, end).maxBy(_.x))
-      Iterator
-        .iterate(startPt)(p => p.copy(x = p.x + 1))
-        .takeWhile(_.x <= endPt.x)
-    } else {
-      throw new IllegalStateException("Non-axis-aligned line")
-    }
+    val xStep = (end.x - start.x).sign
+    val yStep = (end.y - start.y).sign
+
+    Iterator
+      .iterate(start)(p => Pos(p.x + xStep, p.y + yStep))
+      .takeWhile(_ != end)
+      .concat(Iterator(end))
   }
 }
 
+def parseLines: Iterator[Line] = inputLines(5)
+  .map(_.split(" -> ").map(_.split(',').map(_.toInt)))
+  .map { case Array(Array(x1, y1), Array(x2, y2)) =>
+    Line(Pos(x1, y1), Pos(x2, y2))
+  }
+
+// Use sparse map to count number of lines crossing each point
+def countPoints(lines: Iterator[Line]): Int = lines
+  .foldLeft(Map.empty[Pos, Int]) { case (map, line) =>
+    line.coveredPoints.foldLeft(map) { case (map, point) =>
+      map.updatedWith(point) {
+        case None       => Some(1)
+        case Some(prev) => Some(prev + 1)
+      }
+    }
+  }
+  .count(_._2 >= 2)
+
 object Part1 {
   def main(args: Array[String]): Unit = {
-    // Use sparse map to count number of lines crossing each point
-    val pointMap = inputLines(5)
-      .map(_.split(" -> ").map(_.split(',').map(_.toInt)))
-      .map { case Array(Array(x1, y1), Array(x2, y2)) =>
-        Line(Pos(x1, y1), Pos(x2, y2))
-      }
-      .filter(_.isAxisAligned)
-      .foldLeft(Map.empty[Pos, Int]) { case (map, line) =>
-        line.coveredPoints.foldLeft(map) { case (map, point) =>
-          map.updatedWith(point) {
-            case None       => Some(1)
-            case Some(prev) => Some(prev + 1)
-          }
-        }
-      }
+    println(countPoints(parseLines.filter(_.isAxisAligned)))
+  }
+}
 
-    println(pointMap.count(_._2 >= 2))
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    println(countPoints(parseLines))
   }
 }

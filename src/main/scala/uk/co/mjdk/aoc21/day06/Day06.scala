a32869d99455f68e56f79a40069367d3e0aae68d
Matt Jadczak <matt@mjdk.co.uk>
2021-12-06 23:12:31 +0000

    Day 6, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day06/Day06.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day06/Day06.scala
@@ -2,35 +2,43 @@ package uk.co.mjdk.aoc21.day06
 
 import uk.co.mjdk.aoc21.inputLines
 
-object Part1 {
-  def main(args: Array[String]): Unit = {
-    // Represent the number of lanternfish with each timer value by using a map
-    val initialState =
-      inputLines(6)
-        .next()
-        .split(',')
-        .toVector
-        .map(_.toInt)
-        .groupMapReduce(identity)(_ => 1)(_ + _)
+def fishAfterNDays(n: Int): Long = {
+  // Represent the number of lanternfish with each timer value by using a map
+  val initialState =
+    inputLines(6)
+      .next()
+      .split(',')
+      .toVector
+      .map(_.toInt)
+      .groupMapReduce(identity)(_ => 1L)(_ + _)
 
-    val numFish = Iterator
-      .iterate(initialState) { fish =>
-        fish.foldLeft(Map.empty[Int, Int]) { case (newFish, timer -> numFish) =>
-          val update: Option[Int] => Option[Int] =
-            v => Some(v.getOrElse(0) + numFish)
-          timer match {
-            case 0 =>
-              newFish.updatedWith(6)(update).updatedWith(8)(update)
-            case n =>
-              newFish.updatedWith(n - 1)(update)
-          }
+  Iterator
+    .iterate(initialState) { fish =>
+      fish.foldLeft(Map.empty[Int, Long]) { case (newFish, timer -> numFish) =>
+        val update: Option[Long] => Option[Long] =
+          v => Some(v.getOrElse(0L) + numFish)
+        timer match {
+          case 0 =>
+            newFish.updatedWith(6)(update).updatedWith(8)(update)
+          case n =>
+            newFish.updatedWith(n - 1)(update)
         }
       }
-      .map(_.valuesIterator.sum) // counts of fish
-      // 0th value after 0 days, 1st value after 1 day, etc. Drop first 80 values to get state after 80 days.
-      .drop(80)
-      .next()
+    }
+    .map(_.valuesIterator.sum) // counts of fish
+    // 0th value after 0 days, 1st value after 1 day, etc. Drop first 80 values to get state after 80 days.
+    .drop(n)
+    .next()
+}
 
-    println(numFish)
+object Part1 {
+  def main(args: Array[String]): Unit = {
+    println(fishAfterNDays(80))
+  }
+}
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    println(fishAfterNDays(256))
   }
 }

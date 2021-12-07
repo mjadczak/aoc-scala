ff054fd638ad725eda283f4eebb7a4eb9fead2e3
Matt Jadczak <matt@mjdk.co.uk>
2021-12-07 19:46:09 +0000

    Day 7, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day07/Day07.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day07/Day07.scala
@@ -2,6 +2,9 @@ package uk.co.mjdk.aoc21.day07
 
 import uk.co.mjdk.aoc21.inputLines
 
+def crabPositions: Vector[Int] =
+  inputLines(7).next().split(',').iterator.map(_.toInt).toVector
+
 object Part1 {
   def main(args: Array[String]): Unit = {
     // The solution is simply the median of all the numbers - once we have an equal amount of lanternfish either side,
@@ -10,12 +13,32 @@ object Part1 {
 
     // There exist better ways of finding the median, but we can be lazy here
 
-    val nums =
-      inputLines(7).next().split(',').iterator.map(_.toInt).toVector.sorted
+    val crabs = crabPositions.sorted
+
     // we don't need the _actual_ median, just any point between the two middle ones. In particular, for an even number
     // we can take the left middle value
-    val middle = nums((nums.length - 1) / 2)
-    val cost = nums.iterator.map(n => (n - middle).abs).sum
+    val middle = crabs((crabs.length - 1) / 2)
+    val cost = crabs.iterator.map(n => (n - middle).abs).sum
+    println(cost)
+  }
+}
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    // Note the sum of the first n numbers is n(n+1)/2, and write f(x) = the total cost of point x
+    // Then set f'(x) = 0 and solve. We get the mean + a correction factor which we can calculate iteratively:
+    // -(1/2)*mean(sign(x-t))
+
+    val crabs = crabPositions
+    val numCrabs = crabs.length
+    val mean = crabs.sum.toDouble / numCrabs
+    val numSmaller = crabs.count(_ < mean)
+    val correction = .5d * (numSmaller.toDouble / numCrabs.toDouble)
+    val middle = (mean - correction).round
+    val cost = crabs.iterator.map { n =>
+      val dist = (n - middle).abs
+      dist * (dist + 1) / 2
+    }.sum
     println(cost)
   }
 }

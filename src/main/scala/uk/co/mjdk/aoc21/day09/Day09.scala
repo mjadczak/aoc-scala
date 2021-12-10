c03c3999c8436a9d2cb9d7b276b698c3f40c3c28
Matt Jadczak <matt@mjdk.co.uk>
2021-12-10 06:50:04 +0000

    Day 9, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day09/Day09.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day09/Day09.scala
@@ -2,6 +2,8 @@ package uk.co.mjdk.aoc21.day09
 
 import uk.co.mjdk.aoc21.inputLines
 
+import scala.util.chaining._
+
 // One could have an argument about using two Vectors rather than a map, but this is more convenient in this case
 case class Pos(row: Int, col: Int) {
   def adjacentPositions: Set[Pos] = {
@@ -29,11 +31,46 @@ def iterLowPoints(grid: Map[Pos, Int]): Iterator[(Pos, Int)] = grid.iterator
 
 object Part1 {
   def main(args: Array[String]): Unit = {
-    val grid = parseGrid
-
-    val res = iterLowPoints(grid).map { case (_, height) =>
+    val res = iterLowPoints(parseGrid).map { case (_, height) =>
       height + 1
     }.sum
+
+    println(res)
+  }
+}
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    val grid = parseGrid
+    val lowPoints = iterLowPoints(grid).map(_._1).toSet
+
+    // With the way the question is defined, we have a basin per low point, but also we don't need to worry
+    // about ridges of less than 9 height, which makes things simpler. Also, we don't need to worry about tracking
+    // visited cells between different basins.
+
+    def countNeighbours(
+        origPoint: Pos,
+        visited: Set[Pos] = Set.empty
+    ): (Int, Set[Pos]) =
+      // DFS - we need to visit all points so type of search should not matter
+      origPoint.adjacentPositions.iterator
+        .flatMap(p => grid.get(p).map(p -> _))
+        .filter(_._2 < 9)
+        .foldLeft((1, visited + origPoint)) {
+          case ((count, visited), (point, _)) if visited.contains(point) =>
+            (count, visited)
+          case ((count, visited), (point, _)) =>
+            val (nCount, newVisited) =
+              countNeighbours(point, visited)
+            (count + nCount, newVisited)
+        }
+
+    val res = lowPoints.iterator
+      .map(countNeighbours(_)._1)
+      .toVector
+      .sorted(Ordering[Int].reverse)
+      .take(3)
+      .product
 
     println(res)
   }

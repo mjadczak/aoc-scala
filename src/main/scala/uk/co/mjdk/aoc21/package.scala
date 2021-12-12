c17a3d14e4579bf15922be9bdfbc652c5afef5be
Matt Jadczak <matt@mjdk.co.uk>
2021-12-12 19:23:31 +0000

    Refactor input reader

--- src/main/scala/uk/co/mjdk/aoc21/package.scala
+++ src/main/scala/uk/co/mjdk/aoc21/package.scala
@@ -1,16 +1,22 @@
 package uk.co.mjdk
 
 import scala.io.Source
+import scala.util.Using
 
 package object aoc21 {
-  // Normally the stream opened by this function should be closed. In this very particular case we know that the
-  // program is pretty much finished after the stream is consumed, so we don't care in the name of clearer logic.
-  // Tradeoffs like this is why having something like ZIO around is nice, as you can have your cake and eat it too.
-  // Maybe a ZIO version is in the cards - for now, we leak the stream.
+  // Used to stream these in, and just leak the open source, but that bothered me. The inputs are small enough that
+  // we should just read them in wholesale, but it's nice to keep a streaming interface.
   def inputLines(dayNum: Int): Iterator[String] =
-    Source
-      .fromResource(s"uk/co/mjdk/aoc21/input${dayNum.formatted("%02d")}.txt")
-      .getLines()
+    Using
+      .resource(
+        Source
+          .fromResource(
+            s"uk/co/mjdk/aoc21/input${dayNum.formatted("%02d")}.txt"
+          )
+      ) {
+        _.getLines().toVector
+      }
+      .iterator
 
   extension [T](s: Iterable[T]) {
     def only: T = {

223fef95ef457cf47a0110f1338d37bca53bb205
Matt Jadczak <matt@mjdk.co.uk>
2021-12-12 20:20:32 +0000

    [2015] Day 4, Part 2

--- src/main/scala/uk/co/mjdk/aoc15/day04/Day04.scala
+++ src/main/scala/uk/co/mjdk/aoc15/day04/Day04.scala
@@ -11,23 +11,38 @@ val Md5Algo = MessageDigest.getInstance("MD5")
 def md5Digest(str: String): Array[Byte] =
   Md5Algo.digest(str.getBytes(StandardCharsets.UTF_8))
 
-def has5InitialZeroes(ba: Array[Byte]): Boolean = {
-  // 5 zeroes => two 0 bytes and a byte with upper half of 0
-  if (ba.length < 3) {
+def hasZeroes(numZeroes: Int, ba: Array[Byte]): Boolean = {
+  if (ba.length < (numZeroes + 1) / 2) {
     false
   } else {
-    ba(0) == 0 && ba(1) == 0 && (ba(2) & 0xf0) == 0
+    val wholeZeroes = (0 until (numZeroes / 2)).forall(i => ba(i) == 0)
+    if (numZeroes % 2 == 0) {
+      wholeZeroes
+    } else {
+      val lastIndex = numZeroes / 2
+      wholeZeroes && (ba(lastIndex) & 0xf0) == 0
+    }
   }
 }
 
+def findNZeroes(numZeroes: Int): Int =
+  Iterator
+    .from(1)
+    .map(i => (i, md5Digest(s"$Input$i")))
+    .find(x => hasZeroes(numZeroes, x._2))
+    .get
+    ._1
+
 object Part1 {
   def main(args: Array[String]): Unit = {
-    Iterator
-      .from(1)
-      .map(i => (i, md5Digest(s"$Input$i")))
-      .find(x => has5InitialZeroes(x._2))
-      .get
-      ._1
+    findNZeroes(5)
+      .pipe(println)
+  }
+}
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    findNZeroes(6)
       .pipe(println)
   }
 }

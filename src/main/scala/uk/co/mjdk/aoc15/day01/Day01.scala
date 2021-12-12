a9b54f8b71e2ad354926364098bc125b5a5d9486
Matt Jadczak <matt@mjdk.co.uk>
2021-12-12 19:43:35 +0000

    [2015] Day 1, Part 2

--- src/main/scala/uk/co/mjdk/aoc15/day01/Day01.scala
+++ src/main/scala/uk/co/mjdk/aoc15/day01/Day01.scala
@@ -3,17 +3,29 @@ package uk.co.mjdk.aoc15.day01
 import uk.co.mjdk.aoc._
 import scala.util.chaining._
 
+def inputAsNumbers: Iterator[Int] = inputLines(15)(1)
+  .next()
+  .iterator
+  .map {
+    case '(' => 1
+    case ')' => -1
+  }
+
 object Part1 {
   def main(args: Array[String]): Unit = {
-    inputLines(15)(1)
-      .next()
-      .iterator
-      .map {
-        case '(' => 1
-        case ')' => -1
-      }
-      .sum
+    inputAsNumbers.sum
       .pipe(println)
+  }
+}
 
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    inputAsNumbers
+      .scanLeft(0)(_ + _)
+      .zipWithIndex
+      .find(_._1 == -1)
+      .get
+      ._2
+      .pipe(println)
   }
 }

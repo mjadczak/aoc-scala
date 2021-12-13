83f047b524fb8577e50b3b4c85eb8e77be9db801
Matt Jadczak <matt@mjdk.co.uk>
2021-12-13 23:47:50 +0000

    [2015] Day 8, Part 1

---
+++ src/main/scala/uk/co/mjdk/aoc15/day08/Day08.scala
@@ -0,0 +1,38 @@
+package uk.co.mjdk.aoc15.day08
+
+import uk.co.mjdk.aoc.inputLines
+
+import scala.annotation.tailrec
+
+def parseString(input: String): String = parseString(input.toCharArray.toList)
+
+@tailrec
+def parseString(
+    input: List[Char],
+    output: String = ""
+): String = input match {
+  case Nil =>
+    output
+  case '"' :: rest if output.isEmpty || rest.isEmpty =>
+    parseString(rest, output)
+  case '\\' :: '\\' :: rest =>
+    parseString(rest, output + '\\')
+  case '\\' :: '"' :: rest =>
+    parseString(rest, output + '"')
+  case '\\' :: 'x' :: h :: l :: rest =>
+    val char = (Integer
+      .parseInt(h.toString, 16) << 2 + Integer.parseInt(l.toString, 16)).toChar
+    parseString(rest, output + char)
+  case c :: rest =>
+    parseString(rest, output + c)
+}
+
+object Part1 {
+  def main(args: Array[String]): Unit = {
+    val total = inputLines(15)(8).map { line =>
+      val parsed = parseString(line)
+      line.length - parsed.length
+    }.sum
+    println(total)
+  }
+}

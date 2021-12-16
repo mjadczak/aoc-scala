bcd94e2fe06888c7877454985db7f1b11c3d6936
Matt Jadczak <matt@mjdk.co.uk>
2021-12-16 20:45:37 +0000

    [2021] Day 15, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day15/Day15.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day15/Day15.scala
@@ -3,8 +3,9 @@ package uk.co.mjdk.aoc21.day15
 import uk.co.mjdk.aoc.inputLines
 
 import java.util
-import java.util.{PriorityQueue, Comparator}
-import scala.jdk.javaapi.OptionConverters._
+import java.util.{Comparator, PriorityQueue}
+import scala.annotation.tailrec
+import scala.jdk.javaapi.OptionConverters.*
 
 // Dijkstra time! We need a priority queue, and there is no immutable one in Scala's stdlib.
 // There is a mutable one, but it does not support changing priority or removing a specific element.
@@ -22,58 +23,108 @@ case class Pos(row: Int, col: Int) {
   }
 }
 
-def parseGrid: (util.Map[Pos, Int], Pos) = {
-  val map = new util.HashMap[Pos, Int]()
-  var maxCol = 0
-  var maxRow = 0
-  inputLines(21)(15).zipWithIndex
-    .foreach { case (line, row) =>
-      maxRow = maxRow.max(row)
-      line.iterator.zipWithIndex.foreach { case (digit, col) =>
-        maxCol = maxCol.max(col)
-        map.put(Pos(row, col), digit.asDigit)
-      }
+def parseGrid: (Map[Pos, Int], Pos) = {
+  val (map, maxRow, maxCol) = inputLines(21)(15).zipWithIndex
+    .foldLeft((Map.empty[Pos, Int], 0, 0)) {
+      case ((map, maxRow, maxCol), (line, row)) =>
+        val (map2, maxCol2) = line.iterator.zipWithIndex.foldLeft((map, 0)) {
+          case ((map, maxCol), (digit, col)) =>
+            (map + (Pos(row, col) -> digit.asDigit), maxCol.max(col))
+        }
+        (map2, maxRow.max(row), maxCol2.max(maxCol))
     }
   (map, Pos(maxRow, maxCol))
 }
 
-object Day15 {
-  def main(args: Array[String]): Unit = {
-    val startPos = Pos(0, 0)
-    val (grid, targetPos) = parseGrid
-    val distances = new util.HashMap[Pos, Int]()
-    val pq =
-      new PriorityQueue[(Pos, Int)](
-        grid.size(),
-        Comparator.comparingInt[(Pos, Int)](_._2)
-      )
-    grid.keySet().forEach { pos =>
-      if (pos != startPos) {
-        pq.add((pos, Int.MaxValue))
-        distances.put(pos, Int.MaxValue)
-      }
+class VirtualGrid(
+    val rawGrid: Map[Pos, Int],
+    val rawMaxPos: Pos,
+    val expansionFactor: Int = 1
+) {
+  require(expansionFactor >= 1)
+  private val rawWidth = rawMaxPos.row + 1
+  private val rawHeight = rawMaxPos.col + 1
+
+  @tailrec
+  private def wrapValue(value: Int): Int = {
+    if (value <= 9) {
+      value
+    } else {
+      wrapValue(value - 9)
     }
-    pq.add((startPos, 0))
-    distances.put(startPos, 0)
-    val visitedNodes = new util.HashSet[Pos]()
-    while (!visitedNodes.contains(targetPos)) {
-      val (curNode, curMinDistance) = pq.poll()
-      curNode.adjacentPositions
-        .filter(grid.containsKey)
-        .filterNot(visitedNodes.contains)
-        .foreach { adjacentNode =>
-          val tentativeDistance = distances.get(adjacentNode)
-          val candidateDistance = curMinDistance + grid.get(adjacentNode)
-          if (candidateDistance < tentativeDistance) {
-            distances.put(adjacentNode, candidateDistance)
-            pq.remove(
-              (adjacentNode, tentativeDistance)
-            ) // This is not great - a linear scan
-            pq.add((adjacentNode, candidateDistance))
-          }
+  }
+
+  def contains(pos: Pos): Boolean =
+    pos.row <= maxPos.row && pos.col <= maxPos.col && pos.row >= 0 && pos.col >= 0
+  def apply(pos: Pos): Int = {
+    if (!contains(pos)) {
+      throw new NoSuchElementException(f"No entry at $pos")
+    }
+    val rowFactor = pos.row / rawWidth
+    val rowCoord = pos.row % rawWidth
+    val colFactor = pos.col / rawHeight
+    val colCoord = pos.col % rawHeight
+    wrapValue(rawGrid(Pos(rowCoord, colCoord)) + rowFactor + colFactor)
+  }
+  def positions: Iterator[Pos] = 0.to(maxPos.row).iterator.flatMap { row =>
+    0.to(maxPos.col).iterator.map { col =>
+      Pos(row, col)
+    }
+  }
+  val maxPos: Pos =
+    Pos(rawWidth * expansionFactor - 1, rawHeight * expansionFactor - 1)
+}
+
+def findLowestCost(grid: VirtualGrid): Int = {
+  val startPos = Pos(0, 0)
+  val distances = new util.HashMap[Pos, Int]()
+  val targetPos = grid.maxPos
+  val pq =
+    new PriorityQueue[(Pos, Int)](
+      Comparator.comparingInt[(Pos, Int)](_._2)
+    )
+  grid.positions.foreach { pos =>
+    if (pos != startPos) {
+      pq.add((pos, Int.MaxValue))
+      distances.put(pos, Int.MaxValue)
+    }
+  }
+  pq.add((startPos, 0))
+  distances.put(startPos, 0)
+  val visitedNodes = new util.HashSet[Pos]()
+  while (!visitedNodes.contains(targetPos)) {
+    val (curNode, curMinDistance) = pq.poll()
+    curNode.adjacentPositions
+      .filter(grid.contains)
+      .filterNot(visitedNodes.contains)
+      .foreach { adjacentNode =>
+        val tentativeDistance = distances.get(adjacentNode)
+        val candidateDistance = curMinDistance + grid(adjacentNode)
+        if (candidateDistance < tentativeDistance) {
+          distances.put(adjacentNode, candidateDistance)
+          pq.remove(
+            (adjacentNode, tentativeDistance)
+          ) // This is not great - a linear scan
+          pq.add((adjacentNode, candidateDistance))
         }
-      visitedNodes.add(curNode)
-    }
-    println(distances.get(targetPos))
+      }
+    visitedNodes.add(curNode)
+  }
+  distances.get(targetPos)
+}
+
+object Part1 {
+  def main(args: Array[String]): Unit = {
+    val (rawGrid, maxPos) = parseGrid
+    val grid = VirtualGrid(rawGrid, maxPos)
+    println(findLowestCost(grid))
+  }
+}
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    val (rawGrid, maxPos) = parseGrid
+    val grid = VirtualGrid(rawGrid, maxPos, 5)
+    println(findLowestCost(grid))
   }
 }

8a41e8aa10f0a94237d074b227c5504f7425ef4d
Matt Jadczak <matt@mjdk.co.uk>
2021-12-27 16:56:43 +0000

    [2021] Day 21, Part 2

--- src/main/scala/uk/co/mjdk/aoc21/day21/Day21.scala
+++ src/main/scala/uk/co/mjdk/aoc21/day21/Day21.scala
@@ -2,6 +2,9 @@ package uk.co.mjdk.aoc21.day21
 
 import uk.co.mjdk.aoc.inputLines
 
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
 def aggRollValues: Iterator[Int] =
   Iterator
     .continually(1.to(100).iterator)
@@ -31,18 +34,19 @@ case class Player(
 case class State(
     player1: Player,
     player2: Player,
-    numAggregateDiceValuesRead: Int = 0,
     player1Turn: Boolean = true
 ) {
-  def winner: Option[Player] = if (player1.score >= 1000) {
+  def winner(winningScore: Int): Option[Player] = if (
+    player1.score >= winningScore
+  ) {
     Some(player1)
-  } else if (player2.score >= 1000) {
+  } else if (player2.score >= winningScore) {
     Some(player2)
   } else {
     None
   }
 
-  def loser: Option[Player] = winner match {
+  def loser(winningScore: Int): Option[Player] = winner(winningScore) match {
     case Some(`player1`) => Some(player2)
     case Some(`player2`) => Some(player1)
     case None            => None
@@ -51,18 +55,14 @@ case class State(
   def next(aggDiceValue: Int): State = if (player1Turn) {
     copy(
       player1 = player1.movedBy(aggDiceValue),
-      numAggregateDiceValuesRead = numAggregateDiceValuesRead + 1,
       player1Turn = false
     )
   } else {
     copy(
       player2 = player2.movedBy(aggDiceValue),
-      numAggregateDiceValuesRead = numAggregateDiceValuesRead + 1,
       player1Turn = true
     )
   }
-
-  def numDiceRolls: Int = numAggregateDiceValuesRead * 3
 }
 
 def parseInput: (Int, Int) = {
@@ -76,6 +76,84 @@ def parseInput: (Int, Int) = {
 object Part1 {
   def main(args: Array[String]): Unit = {
     val (pos1, pos2) = parseInput
+    val initialState = (
+      State(
+        Player(
+          1,
+          Space(pos1)
+        ),
+        Player(
+          2,
+          Space(pos2)
+        )
+      ),
+      0
+    )
+
+    val (finalState, numDice) = aggRollValues
+      .scanLeft(initialState) { case ((state, numDice), diceRoll) =>
+        (state.next(diceRoll), numDice + 1)
+      }
+      .find(_._1.winner(1000).nonEmpty)
+      .get
+
+    println(finalState.loser(1000).get.score * numDice * 3)
+  }
+}
+
+def playDirac(initialState: State): (Long, Long) =
+  playDirac(Map(initialState -> 1L), 0L, 0L)
+
+def allNextStates(state: State): Iterator[State] = for {
+  r1 <- 1.to(3).iterator
+  r2 <- 1.to(3).iterator
+  r3 <- 1.to(3).iterator
+} yield state.next(r1 + r2 + r3)
+
+// total roll -> number of states
+val allNextRolls: Map[Int, Int] = {
+  val allRolls = for {
+    r1 <- 1.to(3)
+    r2 <- 1.to(3)
+    r3 <- 1.to(3)
+  } yield r1 + r2 + r3
+
+  allRolls.groupMapReduce(identity)(_ => 1)(_ + _)
+}
+
+@tailrec
+def playDirac(
+    inProgress: Map[State, Long],
+    wonPlayer1: Long,
+    wonPlayer2: Long
+): (Long, Long) =
+  if (inProgress.isEmpty) {
+    (wonPlayer1, wonPlayer2)
+  } else {
+    val (finished, unfinished) = inProgress.partition(_._1.winner(21).nonEmpty)
+    val (finished1, finished2) = finished.foldLeft((0L, 0L)) {
+      case ((w1, w2), (state, num)) =>
+        state.winner(21) match {
+          case Some(state.player1) => (w1 + num, w2)
+          case Some(state.player2) => (w1, w2 + num)
+        }
+    }
+
+    val newInProgress = unfinished.iterator
+      .flatMap { case (state, num) =>
+        allNextRolls.map { case (nextRoll, numNext) =>
+          state.next(nextRoll) -> num * numNext
+        }
+      }
+      .toVector
+      .groupMapReduce(_._1)(_._2)(_ + _)
+
+    playDirac(newInProgress, wonPlayer1 + finished1, wonPlayer2 + finished2)
+  }
+
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    val (pos1, pos2) = parseInput
     val initialState = State(
       Player(
         1,
@@ -87,13 +165,9 @@ object Part1 {
       )
     )
 
-    val finalState = aggRollValues
-      .scanLeft(initialState) { case (state, diceRoll) =>
-        state.next(diceRoll)
-      }
-      .find(_.winner.nonEmpty)
-      .get
+    val (won1, won2) = playDirac(initialState)
 
-    println(finalState.loser.get.score * finalState.numDiceRolls)
+    println((won1, won2))
+    println(won1.max(won2))
   }
 }
